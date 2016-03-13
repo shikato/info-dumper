d594ff38ae47f78923acc5c92bfa91634322febe
shikato <nnilan2@gmail.com>
2016-03-13 20:21:09 +0900

    changed module name & added sample app #9

---
+++ sample/src/main/java/org/shikato/indodumper/sample/MainActivity.java
@@ -0,0 +1,15 @@
+package org.shikato.indodumper.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.shikato.sampleapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
